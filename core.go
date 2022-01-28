161dce03accbf19663d136a014fa4bca78a8de4f
ppaydd <736420641@qq.com>
2022-01-28 15:44:37 +0800

    gmm algorithm

--- core.go
+++ core.go
@@ -4,6 +4,11 @@ package vlfeat
 #include <stdlib.h>
 */
 import "C"
+import (
+	"errors"
+	"reflect"
+	"unsafe"
+)
 
 type VlErrorType int
 
@@ -16,8 +21,22 @@ const (
 	VlErrorEOF      VlErrorType = 5
 )
 
-// img data switch to C
+type VlType uint
 
+const (
+	VlTypeFloat  VlType = 1
+	VlTypeDouble VlType = 2
+	VlTypeInt8   VlType = 3
+	VlTypeUint8  VlType = 4
+	VlTypeInt16  VlType = 5
+	VlTypeUint16 VlType = 6
+	VlTypeInt32  VlType = 7
+	VlTypeUint32 VlType = 8
+	VlTypeInt64  VlType = 9
+	VlTypeUint64 VlType = 10
+)
+
+// img data switch to C
 func toCFloatArrayPtr(img []float32) *C.float {
 	datas := make([]C.float, len(img))
 	for i, goData := range img {
@@ -32,4 +51,75 @@ func toCUcharArrayPtr(img []uint8) *C.uchar {
 		datas[i] = C.uchar(goData)
 	}
 	return &datas[0]
+}
+
+func ToCVlTypeArrayPtr(data interface{}, vlType VlType) (unsafe.Pointer, arrayLength, error) {
+	switch reflect.TypeOf(data).Kind() {
+	case reflect.Slice, reflect.Array:
+		slice := reflect.ValueOf(data)
+		length := slice.Len()
+		switch vlType {
+		case VlTypeFloat:
+			cData := make([]C.float, length)
+			for i := 0; i < length; i++ {
+				cData[i] = C.float(slice.Index(i).Float())
+			}
+			return unsafe.Pointer(&cData), length, nil
+		case VlTypeDouble:
+			cData := make([]C.double, length)
+			for i := 0; i < length; i++ {
+				cData[i] = C.double(slice.Index(i).Float())
+			}
+			return unsafe.Pointer(&cData), length, nil
+		case VlTypeInt8:
+			cData := make([]C.char, length)
+			for i := 0; i < length; i++ {
+				cData[i] = C.char(slice.Index(i).Int())
+			}
+			return unsafe.Pointer(&cData), length, nil
+		case VlTypeUint8:
+			cData := make([]C.uchar, length)
+			for i := 0; i < length; i++ {
+				cData[i] = C.uchar(slice.Index(i).Int())
+			}
+			return unsafe.Pointer(&cData), length, nil
+		case VlTypeInt16:
+			cData := make([]C.short, length)
+			for i := 0; i < length; i++ {
+				cData[i] = C.short(slice.Index(i).Int())
+			}
+			return unsafe.Pointer(&cData), length, nil
+		case VlTypeUint16:
+			cData := make([]C.ushort, length)
+			for i := 0; i < length; i++ {
+				cData[i] = C.ushort(slice.Index(i).Int())
+			}
+			return unsafe.Pointer(&cData), length, nil
+		case VlTypeInt32:
+			cData := make([]C.int, length)
+			for i := 0; i < length; i++ {
+				cData[i] = C.int(slice.Index(i).Int())
+			}
+			return unsafe.Pointer(&cData), length, nil
+		case VlTypeUint32:
+			cData := make([]C.uint, length)
+			for i := 0; i < length; i++ {
+				cData[i] = C.uint(slice.Index(i).Int())
+			}
+			return unsafe.Pointer(&cData), length, nil
+		case VlTypeInt64:
+			cData := make([]C.longlong, length)
+			for i := 0; i < length; i++ {
+				cData[i] = C.longlong(slice.Index(i).Int())
+			}
+			return unsafe.Pointer(&cData), length, nil
+		case VlTypeUint64:
+			cData := make([]C.ulonglong, length)
+			for i := 0; i < length; i++ {
+				cData[i] = C.ulonglong(slice.Index(i).Int())
+			}
+			return unsafe.Pointer(&cData), length, nil
+		}
+	}
+	return nil, 0, errors.New("must be support slice or array")
 }
---
+++ gmm.go
@@ -0,0 +1,235 @@
+package vlfeat
+
+/*
+#include <stdlib.h>
+#include <gmm.h>
+*/
+import "C"
+import (
+	"reflect"
+	"unsafe"
+)
+
+type VlGMMInitialization int
+
+const (
+	VlGMMKMeans VlGMMInitialization = 0
+	VlGMMRand   VlGMMInitialization = 1
+	VlGMMCustom VlGMMInitialization = 2
+)
+
+type GMM struct {
+	p *C.VlGMM
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#afe0bdce1cf97a7b64011ae58bc8b9697
+func NewGMM(dataType VlType, dimension, numComponents uint) GMM {
+	p := C.vl_gmm_new(C.uint(dataType), C.uint(dimension), C.uint(numComponents))
+	return GMM{p: p}
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a2c6889a0569271e72096d18735f28211
+func (gmm *GMM) Copy() GMM {
+	p := C.vl_gmm_new_copy(gmm.p)
+	return GMM{p: p}
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#adbde533172047f780e2713d18387a9d0
+func (gmm *GMM) Delete() {
+	C.vl_gmm_delete(gmm.p)
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a1baeb175e0cdb68c548addc837d7baae
+func (gmm *GMM) Reset() {
+	C.vl_gmm_reset(gmm.p)
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a856cdfe758b7c48c3309859853f38e36
+func (gmm *GMM) Cluster(data interface{}) (float64, error) {
+	vltype := gmm.GetDataType()
+	dataPtr, length, err := ToCVlTypeArrayPtr(data, vltype)
+	if err != nil {
+		return 0, err
+	}
+	return float64(C.vl_gmm_cluster(gmm.p, dataPtr, C.uint(length))), nil
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#aab9d461e2fca63960f2751ae86946804
+func (gmm *GMM) InitWithRandData(data interface{}) error {
+	vltype := gmm.GetDataType()
+	dataPtr, length, err := ToCVlTypeArrayPtr(data, vltype)
+	if err != nil {
+		return err
+	}
+	C.vl_gmm_init_with_rand_data(gmm.p, dataPtr, C.uint(length))
+	return nil
+}
+
+// used other algorithm, delay first
+func (gmm *GMM) InitWithKmeans() {
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a4f8f3fc91d284a2866fc5fd5d5b48dfd
+func (gmm *GMM) Em(data interface{}) (float64, error) {
+	vltype := gmm.GetDataType()
+	dataPtr, length, err := ToCVlTypeArrayPtr(data, vltype)
+	if err != nil {
+		return 0, err
+	}
+	em := C.vl_gmm_em(gmm.p, dataPtr, C.uint(length))
+	return float64(em), nil
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a001f4a994fbb997e7b7b38d56e69e495
+func (gmm *GMM) SetMeans(means interface{}) error {
+	vltype := gmm.GetDataType()
+	dataPtr, _, err := ToCVlTypeArrayPtr(means, vltype)
+	if err != nil {
+		return err
+	}
+	C.vl_gmm_set_means(gmm.p, dataPtr)
+	return nil
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a9467941c38e0eb70f1e7de2cf8242716
+func (gmm *GMM) SetCovariances(covariances interface{}) error {
+	vltype := gmm.GetDataType()
+	dataPtr, _, err := ToCVlTypeArrayPtr(covariances, vltype)
+	if err != nil {
+		return err
+	}
+	C.vl_gmm_set_covariances(gmm.p, dataPtr)
+	return nil
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a4c0e4759f7b082400cfe4da0991139c8
+func (gmm *GMM) SetPriors(priors interface{}) error {
+	vltype := gmm.GetDataType()
+	dataPtr, _, err := ToCVlTypeArrayPtr(priors, vltype)
+	if err != nil {
+		return err
+	}
+	C.vl_gmm_set_priors(gmm.p, dataPtr)
+	return nil
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a9801c187ad6f0561275a715d3e589a59
+func (gmm *GMM) SetNumRepetitions(numRepetitions uint) {
+	C.vl_gmm_set_num_repetitions(gmm.p, C.uint(numRepetitions))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a606ce33d200101fa6a60e10a3e89cec1
+func (gmm *GMM) SetMaxNumIterations(maxNumIterations uint) {
+	C.vl_gmm_set_max_num_iterations(gmm.p, C.uint(maxNumIterations))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a96708e3c10107c49f136a0fe1082684a
+func (gmm *GMM) SetVerbosity(verbosity int) {
+	C.vl_gmm_set_verbosity(gmm.p, C.int(verbosity))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a3f34a10ef70b880a81eabce9fc29cc2e
+func (gmm *GMM) SetInitialization(init VlGMMInitialization) {
+	C.vl_gmm_set_verbosity(gmm.p, C.VlGMMInitialization(init))
+}
+
+func (gmm *GMM) SetKmeansInitObject() {}
+
+// https://www.vlfeat.org/api/gmm_8c.html#ac5b9aa600e348e99907cdb9f160c8d1d
+func (gmm *GMM) SetCovarianceLowerBounds(bounds []float64) {
+	cBounds := make([]C.double, len(bounds))
+	for i, bound := range bounds {
+		cBounds[i] = C.float(bound)
+	}
+	C.vl_gmm_set_covariance_lower_bounds(gmm.p, &cBounds[0])
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a607ba2f82f3bfe5949f71acca0d332c8
+func (gmm *GMM) SetCovarianceLowerBound(bound float64) {
+	C.vl_gmm_set_covariance_lower_bound(gmm.p, C.double(bound))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#ae8598b36a92ca066e26e03ea46dee466
+func (gmm *GMM) GetMeans() unsafe.Pointer {
+	return C.vl_gmm_get_means(gmm.p)
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a319648cad7d83d2a31c7beb79b1ba125
+func (gmm *GMM) GetCovariances() unsafe.Pointer {
+	return C.vl_gmm_get_covariances(gmm.p)
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a556605dddee3aa3f2c35898436c5e811
+func (gmm *GMM) GetPriors() unsafe.Pointer {
+	return C.vl_gmm_get_priors(gmm.p)
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a54b1da9397fdb22c1036690e80816e76
+func (gmm *GMM) GetPosteriors() unsafe.Pointer {
+	return C.vl_gmm_get_posteriors(gmm.p)
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#ac8980d73a2bac3f4aba64108ea9b7986
+func (gmm *GMM) GetDataType() VlType {
+	return VlType(C.vl_gmm_get_data_type(gmm.p))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a5dd301238834c161a8ca142f32b2a83c
+func (gmm *GMM) GetDimension() uint {
+	return uint(C.vl_gmm_get_dimension(gmm.p))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#aba79f6a785a9c33b84f5dc38c9b670aa
+func (gmm *GMM) GetNumRepetitions() uint {
+	return uint(C.vl_gmm_get_num_repetitions(gmm.p))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a83a3fb14322069092b4618bcabaf5fda
+func (gmm *GMM) GetNumData() uint {
+	return uint(C.vl_gmm_get_num_data(gmm.p))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a6d4f30630fd47af3dec762a58df7b653
+func (gmm *GMM) GetNumClusters() uint {
+	return uint(C.vl_gmm_get_num_clusters(gmm.p))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a618c40e1338f2433505f83d0745fce7f
+func (gmm *GMM) GetLoglikelihood() float64 {
+	return float64(C.vl_gmm_get_loglikelihood(gmm.p))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a167e3a1208bc756e1681dfc44dab5928
+func (gmm *GMM) GetVerbosity() int {
+	return int(C.vl_gmm_get_verbosity(gmm.p))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#aaa311e17e0190fa3a247349d4e127e3d
+func (gmm *GMM) GetMaxNumIterations() int {
+	return int(C.vl_gmm_get_max_num_iterations(gmm.p))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a51e3a8e372e5d019c215d126823986b0
+func (gmm *GMM) GetInitialization() VlGMMInitialization {
+	return VlGMMInitialization(C.vl_gmm_get_initialization(gmm.p))
+}
+
+// https://www.vlfeat.org/api/gmm_8c.html#a6e149ef463f029ba96788f0aa7442025
+func (gmm *GMM) GetCovarianceLowerBounds() []float64 {
+	cBounds := C.vl_gmm_get_covariance_lower_bounds(gmm.p)
+	length := int(gmm.GetDimension())
+	hdr := reflect.SliceHeader{
+		Data: uintptr(unsafe.Pointer(cBounds)),
+		Len:  int(length),
+		Cap:  int(length),
+	}
+	cBoundSlice := *(*[]C.double)(unsafe.Pointer(&hdr))
+
+	bounds := make([]float64, length)
+	for i, bound := range cBoundSlice {
+		bounds[i] = float64(bound)
+	}
+	return bounds
+}
+
+func (gmm *GMM) GetKmeansInitObject() {}
